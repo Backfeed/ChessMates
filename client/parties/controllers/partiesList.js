8b47702686a7fd8fc15fe2abf604faf0bcda282b
Yaniv Keinan <yanivkeinan@gmail.com>
2015-06-27 15:39:41 +0300

    board added to party

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -85,10 +85,10 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor', '
       position: 'start',
       showNotation: false
     });
-    var board2 = new ChessBoard('board2', {
-      position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R',
-      showNotation: false
-    });
+    //var board2 = new ChessBoard('board2', {
+    //  position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R',
+    //  showNotation: false
+    //});
     var board3 = new ChessBoard('board3', {
       position: 'r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1',
       showNotation: false
