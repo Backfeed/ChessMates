5bc94423c76910232d79a00f56f212415bcbfeb8
Yaniv Keinan <yanivkeinan@gmail.com>
2015-06-28 09:28:41 +0300

    Deploying on Ubuntu

--- provision.sh
+++ provision.sh
@@ -9,3 +9,7 @@ sudo apt-get update
 
 # install Meteor
 curl https://install.meteor.com/ | sh
+
+# issue when deplying on ubuntu
+# might need to add to /usr/local/bin/meteor
+#export LC_ALL="en_US.UTF-8"
