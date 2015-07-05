84d5eb087c3ce5c60bb2c5128b18cbe77924bd2d
Adam Goldman <adamgoldman7@gmail.com>
2015-07-05 19:42:20 +0300

    games cleanup + allow only one suggestion per player per turn

--- client/games/games.ng.js
+++ client/games/games.ng.js
@@ -13,37 +13,59 @@ function config($stateProvider){
 }
 
 function GamesController($scope, $meteor, Engine) {
+  var gameId = "1"; // TODO: Get dynamically from current game
+  
+  angular.extend($scope, {
+    fen            : 'start',
+    selected_move  : {},
+    Engine : Engine, // DEV ONLY
+    isCurrentUserPlayed: isCurrentUserPlayed,
+    foo: {},
+    game: $meteor.object(Games, { game_id: gameId }).subscribe('games'),
+    executeMove: executeMove,
+    restart: restart // DEV ONLY
+  });
+
+  $scope.$watch('game.fen', function(){
+    if ($scope.game.fen && $scope.fen !== $scope.game.fen)
+    {
+      $scope.foo.game.load($scope.game.fen);
+      $scope.foo.board.position($scope.game.fen);
+      $scope.fen = $scope.game.fen;
+    }
+  });
+
+  //TODO why not inject a service here? could we avoid broadcasting data to the whole app?
+  $scope.$on('singleMove', singleMove);
+  $scope.$on('suggestedMovesSelected', suggestedMovesSelected);
+  $scope.$on('angularStockfish::bestMove', onAIMove);
+
+  function isCurrentUserPlayed() {
+    var flag = false;
+    $scope.game.suggested_moves.forEach(function(move){
+      if (move.user_id === $scope.currentUser._id) { flag = true; }
+    });
+    return flag;
+  }
+
   // For development
-  $scope.Engine = Engine;
-  // avoid binding break
-  $scope.foo = {};
-  // For development
-  $scope.restart = function() {
+  function restart () {
     $scope.foo.game.reset();
     $scope.foo.board.position('start');
     $scope.game.fen = 'start';
     $scope.game.pgn = '';
     $scope.game.turns = [];
     $scope.game.suggested_moves = [];
-  };
+  }
 
-  $scope.playAI = function() {
+  function executeMove() {
     //TODO fix this. For now pressing the button will play the first selected move(for dev)
     $scope.foo.game.move($scope.game.suggested_moves[0].notation);
     $scope.foo.board.position($scope.foo.game.fen());
     Engine.getMove($scope.foo.game.history({ verbose: true }).map(function(move){ return move.from + move.to }).join(" "));
-  };
+  }
 
-  var gameId = "1";
-
-  angular.extend($scope, {
-    fen            : 'start',
-    selected_move  : {}
-  });
-
-  $scope.game = $meteor.object(Games, { game_id: gameId }).subscribe('games');
-
-  function onBestMove(e, from, to, promotion) {
+  function onAIMove(e, from, to, promotion) {
     $scope.foo.game.move({ from: from, to: to, promotion: promotion });
     $scope.foo.board.position($scope.foo.game.fen());
     $scope.fen = $scope.foo.game.fen(); // save for returning the piece to before suggestion position
@@ -52,31 +74,26 @@ function GamesController($scope, $meteor, Engine) {
     logTurn();
   }
 
-  //TODO why not inject a service here? could we avoid broadcasting data to the whole app?
-  $scope.$on('singleMove', singleMove);
-  $scope.$on('suggestedMovesSelected', suggestedMovesSelected);
-  $scope.$on('angularStockfish::bestMove', onBestMove);
-
   function singleMove(e) {
-    //TODO validate that this is the first time the player makes a suggestion
-    var notation = getNotation($scope.foo.game.pgn());
-    var newMove = {
+    if (!isCurrentUserPlayed()) {
+      $scope.game.suggested_moves.push({
         user_id: Meteor.userId(),
-        notation: notation,
+        notation: getNotation($scope.foo.game.pgn()),
         avg_stars: '4.5',
         created_at: Date.now(),
         fen: $scope.foo.game.fen(),
         comments: []
-    };
-    $scope.game.suggested_moves.push(newMove);
+      });
+      alert('success on suggesting a move');
+    } else {
+      alert('Can only suggest one move per turn');
+    }
 
-    //TODO alert the user that the move has been suggested
-    alert('success on suggesting a move');
     //TODO move the piece back in a more elegant way
     $scope.foo.game.undo();
     $scope.foo.board.position($scope.fen);
-
   }
+
   //TODO only highlight
   function suggestedMovesSelected(e, move) {
     $scope.selected_move = move;
@@ -95,14 +112,5 @@ function GamesController($scope, $meteor, Engine) {
   function getNotation(pgn){
     return pgn.slice(pgn.lastIndexOf('.')+2, pgn.length);
   }
-
-  $scope.$watch('game.fen', function(){
-    if ($scope.game.fen && $scope.fen !== $scope.game.fen)
-    {
-      $scope.foo.game.load($scope.game.fen);
-      $scope.foo.board.position($scope.game.fen);
-      $scope.fen = $scope.game.fen;
-    }
-  });
 
 }
