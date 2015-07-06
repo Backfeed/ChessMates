5a96157a0de3e0d505cd4bcd9343d23084262ac5
Adam Goldman <adamgoldman7@gmail.com>
2015-07-06 03:10:32 +0300

    Common Service added to DI

--- client/utilities/utilities.ng.js
+++ client/utilities/utilities.ng.js
@@ -1,6 +1,9 @@
 angular.module('blockchess.utilities', [
-  'blockchess.utilities.suggestedMoves',
+  // Services
+  'blockchess.utilities.common',
   'blockchess.utilities.engine',
+  // Directives
+  'blockchess.utilities.suggestedMoves',
   'blockchess.utilities.suggestedMovesPanel',
   'blockchess.utilities.comments',
   'blockchess.utilities.timer'
