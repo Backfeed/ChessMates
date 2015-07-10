b909a6dcca841469c2200e1f3054acc45d8cdb8a
Adam Goldman <adamgoldman7@gmail.com>
2015-07-10 09:05:20 +0300

    suggested move modal - Close on endtime

--- client/utilities/suggest_move_modal/suggest_move_modal.ng.js
+++ client/utilities/suggest_move_modal/suggest_move_modal.ng.js
@@ -1,18 +1,27 @@
 angular.module('blockchess.utilities.suggestMoveModal', [])
 .controller('suggestMoveModalController', suggestMoveModalController);
 
-function suggestMoveModalController($mdDialog) {
+function suggestMoveModalController($mdDialog, CommonService) {
   var ctrl = this;
+
   angular.extend(ctrl, {
     submit: submit,
     cancel: cancel
   });
 
+  whosTurnStream.on('turnChanged', turnChanged);
+
   function submit() {
     $mdDialog.hide(ctrl.stars);
   }
 
-  function cancel() {
-    $mdDialog.cancel('user canceled');
+  function cancel(msg) {
+    msg = msg || 'user canceled';
+    $mdDialog.cancel(msg);
+  }
+
+  function turnChanged() {
+    CommonService.toast("Time's up!");
+    cancel("time ended");
   }
 }
