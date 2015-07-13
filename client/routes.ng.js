aebe553f51a1fe349b9e76e88fc468812e5d1f8f
Adam Goldman <adamgoldman7@gmail.com>
2015-07-13 16:29:30 +0300

    routes code styling

--- client/routes.ng.js
+++ client/routes.ng.js
@@ -1,17 +1,18 @@
-angular.module("blockchess").run(["$rootScope", "$location", function($rootScope, $location) {
-  $rootScope.$on("$stateChangeError", function(event, next, previous, error) {
-        // We can catch the error thrown when the $requireUser promise is rejected
-        // and redirect the user back to the main page
-        if (error === "AUTH_REQUIRED") {
-          $location.path("/games");
-        }
-      });
-}]);
-
 angular.module("blockchess.routes", [])
+.run(stateChangeError)
 .config(routes);
 
 function routes($urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise("/games");
+}
+
+function stateChangeError($rootScope, $state) {
+  $rootScope.$on("$stateChangeError", function(event, next, previous, error) {
+    // We can catch the error thrown when the $requireUser promise is rejected
+    // and redirect the user back to the main page
+    if (error === "AUTH_REQUIRED") {
+      $state.go("games");
+    }
+  });
 }
