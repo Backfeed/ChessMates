c5ebb03b21233361e8e5780ae4bb742c979742cc
Adam Goldman <adamgoldman7@gmail.com>
2015-07-28 05:36:39 +0300

    favorite count fix

--- client/game/suggested_moves/favorite_count.flt.ng.js
+++ client/game/suggested_moves/favorite_count.flt.ng.js
@@ -1,17 +1,14 @@
 angular.module('blockchess.game.suggestedMoves.favoriteCount', [])
 .filter('favoriteCount', favoriteCount);
 
-function favoriteCount() {
-  return function(move) {
-    return getFavoriteCountFrom(move);
+function favoriteCount($meteor) {
+  return function(moveId) {
+    var count = 0;
+    var evaluations = $meteor.collection(Evaluations, { suggestedMoveId: moveId });
+    _.each(evaluations, function(evl) {
+      if (evl.favoriteMove)
+        count += 1;
+    });
+    return count;
   };
-}
-
-function getFavoriteCountFrom(move) {
-  var count = 0;
-  move.evaluations.forEach(function(evl) {
-    if (evl.favoriteMove)
-      count += 1;
-  });
-  return count;
 }
