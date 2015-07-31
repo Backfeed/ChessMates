7d5b707465771abe4d3ad7d5933f079878962fa4
Adam Goldman <adamgoldman7@gmail.com>
2015-07-31 10:35:25 +0300

    Common.js refactor

--- server/lib/Common.js
+++ server/lib/Common.js
@@ -5,18 +5,16 @@ Common = (function Common() {
     displayNameOf: displayNameOf
   };
 
-  function displayNameOf(user) {
-    if (!user) return;
+  function displayNameOf(userOrId) {
+    if (!userOrId) return;
+    user = typeof userOrId === 'string' ? getUserBy(userOrId) : userOrId;
     if (user.profile && user.profile.name) return user.profile.name;
     if (user.emails) return user.emails[0].address;
     return "Anonymous user";
   }
 
   function displayNamesOf(usersIds) {
-    return usersIds.map(function(userId) {
-      return Common.displayNameOf(Meteor.users.findOne(userId));
-    });
+    return _.map(usersIds, displayNameOf);
   }
-
 
 })();
