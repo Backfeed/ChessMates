971018caf2b546f7879ba9268013fd80a877a0e2
Adam Goldman <adamgoldman7@gmail.com>
2015-08-04 16:23:44 +0300

    suggested move cost 1 token

--- server/model/suggested_moves.js
+++ server/model/suggested_moves.js
@@ -1,5 +1,26 @@
 Meteor.publish('suggestedMoves', function (options, gameId, turnIndex) {
-  if (!turnIndex)
-    turnIndex = 1;
+  turnIndex = turnIndex || 1;
   return SuggestedMoves.find({"gameId": "1", "turnIndex": turnIndex});
 });
+
+SuggestedMoves.before.insert(beforeInsert);
+SuggestedMoves.after.insert(afterInsert);
+
+function beforeInsert(userId, move) {
+  if (!userId) return;
+  validateSufficientTokens(userId);
+}
+
+function afterInsert(userId, move) {
+  if (!userId) return;
+  Meteor.users.update(
+    { _id: userId },
+    { $inc: { tokens: -1 } }
+  );
+}
+
+function validateSufficientTokens(userId) {
+  var userTokens = getUserBy(userId).tokens;
+  if (userTokens < 1)
+    throw new Meteor.Error(404, 'Insufficient funds: Suggesting a move costs 1 token, you have ' + userTokens + 'tokens');
+}
