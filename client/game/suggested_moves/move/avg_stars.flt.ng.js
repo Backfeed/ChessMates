d514cd4ec66f94a2ce2a8da79b97ba01047b6211
Adam Goldman <adamgoldman7@gmail.com>
2015-08-07 18:55:07 +0300

    refactored avgStars

--- client/game/suggested_moves/move/avg_stars.flt.ng.js
+++ client/game/suggested_moves/move/avg_stars.flt.ng.js
@@ -3,11 +3,6 @@ angular.module('blockchess.game.suggestedMoves.move.avgStars', [])
 
 function avgStars($meteor) { 
   return function(evaluations) {
-    if (!evaluations || !evaluations.length) return 0;
-    var starsSum = _.reduce(evaluations, function(memo, evl) {
-      return memo + evl.stars;
-    }, 0);
-    var starsAvg = starsSum / evaluations.length;
-    return parseFloat(starsAvg.toFixed(1));
-  }
+    return compose(decimel(1), average, map(property('stars')))(evaluations);
+  };
 }
