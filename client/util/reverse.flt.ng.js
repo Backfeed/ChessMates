ee34982ea2ed540e2d278aca35bd3ae5596bdb4b
Adam Goldman <adamgoldman7@gmail.com>
2015-08-13 19:39:07 +0300

    fix(reverse): fix crash when no input

--- client/util/reverse.flt.ng.js
+++ client/util/reverse.flt.ng.js
@@ -2,5 +2,8 @@ angular.module('blockchess.util.reverse', [])
 .filter('reverse', Reverse);
 
 function Reverse() {
-  return R.reverse;
+  return function(arr) {
+    if (!arr || !arr.length) return null;
+    return R.reverse(arr);
+  }
 }
