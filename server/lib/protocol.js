6e70bb26d7cbc70b988262106a637744ce191a14
Adam Goldman <adamgoldman7@gmail.com>
2015-08-19 03:13:55 +0300

    fix(backfeed): fix proto rate

--- server/lib/protocol.js
+++ server/lib/protocol.js
@@ -1,18 +1,51 @@
-var dummyStake = 3; // Arbitrarily
+// TODO :: Make proper meteor constants
+BASE_STAKE = 0.1;
+BASE_FUNDS = 10;
+// var dummyStake = 3; // Arbitrarily
 
 Meteor.methods({
   protoRate: protoRate,
   protoEndTurn: protoEndTurn
 });
 
-function protoRate(uid, moveId, stars) {
-  var user = F.getUserBy(uid);
-  var stake = Protocol.getStakeBy(user);
-  payReputationAtStake(user, stake);
+function log(msg) {
+  console.log("PROTO: " + msg);
+}
 
-  var evals = Protocol.getEvalsBy(moveId, stars);
-  var fullstake = Protocol.calcFullStake(evals) + dummyStake;
-  Protocol.distributeStakeToEvaluators(evals, stake, fullstake);
+function protoRate(uid, moveId, stars) {
+  // user should lose rep
+  var uStake = calcUserStake(uid); // 1
+  var uRep = User.getRep(uid) - uStake; // 10
+
+  Meteor.users.update({ _id: uid }, { $set: { reputation: uRep } });
+
+  var evaluators = getEvaluators(moveId);
+
+  var multiplier = uStake / calcFullStake(stars, evaluators);
+  
+  _.each(evaluators, function(u) {
+    Meteor.users.update(u._id, { $inc: { reputation: u.reputation * multiplier } });
+  });
+
+  SuggestedMoves.update({ _id: moveId }, { $inc: { reputation: BASE_FUNDS * multiplier } });
+
+}
+
+function calcFullStake(stars, evaluators) {
+  // TODO :: Implement stars
+  return R.compose(R.add(BASE_FUNDS), F.sumBy('reputation'))(evaluators);
+}
+
+function getEvaluators(moveId) {
+  return R.compose(F.mapUsersBy ,F.mapUids, getEvaluations)(moveId);
+}
+
+function getEvaluations(moveId) {
+  return Evaluations.find({ moveId: moveId });
+}
+
+function calcUserStake(uid) {
+  return R.compose(R.multiply(BASE_STAKE), User.getRep)(uid);
 }
 
 function protoEndTurn(gameId, turnIndex) {
@@ -28,8 +61,8 @@ function protoEndTurn(gameId, turnIndex) {
 
 
 /********* Helper methods *********/
-function payReputationAtStake(user, stake) {
-  R.compose(Protocol.updateReputation, addToRep(-stake))(user);
+function payReputationAtStake(user, uStake) {
+  R.compose(Protocol.updateReputation, addToRep(-uStake))(user);
 }
 
 // getMoveBy :: String -> Object
