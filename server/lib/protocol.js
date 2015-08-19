3c12373c44fa381645e295de09e983ab8d8988f2
Adam Goldman <adamgoldman7@gmail.com>
2015-08-19 04:52:54 +0300

    refcator(backfeed): delegate implementations to services

--- server/lib/protocol.js
+++ server/lib/protocol.js
@@ -1,23 +1,18 @@
 // TODO :: Make proper meteor constants
 BASE_STAKE = 0.1;
 BASE_FUNDS = 10;
-// var dummyStake = 3; // Arbitrarily
 
 Meteor.methods({
   protoRate: protoRate,
   protoEndTurn: protoEndTurn
 });
 
-function log(msg) {
-  console.log("PROTO: " + msg);
-}
-
 function protoRate(uid, moveId, stars) {
   // user should lose rep
   var uStake = calcUserStake(uid); // 1
   var uRep = User.getRep(uid) - uStake; // 10
 
-  User.updateRep(uid, uRep);
+  User.set(uid, 'reputation', uRep);
 
   var evaluators = getEvaluators(moveId);
 
@@ -25,11 +20,23 @@ function protoRate(uid, moveId, stars) {
   
   _.each(evaluators, distributeStake);
 
-  SugMov.incRep(moveId, BASE_FUNDS * multiplier);
+  SugMov.inc(moveId, "reputation", BASE_FUNDS * multiplier);
 
   function distributeStake(u) {
-    User.incRep(u._id, u.reputation * multiplier);
+    var amountToInc = u.reputation * multiplier;
+    User.inc(u._id, 'reputation', amountToInc);
   }
+}
+
+function protoEndTurn(gameId, turnIndex) {
+  var winningMove = R.compose(max(R.prop('value')), R.map(Protocol.getMoveStats), SugMov.getBy)(gameId, turnIndex);
+  User.inc(winningMove.uid, 'tokens', winningMove.tokens);
+
+  return { 
+    from: winningMove.notation.substr(0,2), 
+    to: winningMove.notation.substr(2) 
+  };
+  
 }
 
 function calcFullStake(stars, evaluators) {
@@ -49,52 +56,8 @@ function calcUserStake(uid) {
   return R.compose(R.multiply(BASE_STAKE), User.getRep)(uid);
 }
 
-function protoEndTurn(gameId, turnIndex) {
-  var winningMove = R.compose(max(R.prop('value')), R.map(Protocol.getMoveStats), getSuggestedMove)(gameId, turnIndex);
-  awardWinner(winningMove);
 
-  return { 
-    from: winningMove.notation.substr(0,2), 
-    to: winningMove.notation.substr(2) 
-  };
-  
-}
-
-
-/********* Helper methods *********/
-function payReputationAtStake(user, uStake) {
-  R.compose(Protocol.updateReputation, addToRep(-uStake))(user);
-}
-
-// getMoveBy :: String -> Object
-function getMoveBy(moveId) {
-  return SuggestedMoves.findOne({ _id: moveId });
-}
-
-// getNotationBy :: String -> String
-function getNotationBy(moveId) {
-  return getMoveBy(moveId).notation;
-}
-
-// :: String, Number -> [Object]
-function getSuggestedMove(gameId, turnIndex) {
-  return SuggestedMoves.find({ gameId: gameId, turnIndex: turnIndex }).fetch();
-}
-
-// calcAvgMoveVal :: Number, Number -> Object
-function calcAvgMoveVal(totalRep, score) {
-  return {
-    reputation: totalRep, 
-    credits: score / totalRep, 
-    value: score
-  };
-}
-
-// getWinnerUser :: [Object], String -> Object
-function getWinnerUser(moves, winningNotation) {
-  return R.compose(F.getUserBy, toUid, find(isNotationEquals(winningNotation)))(moves);
-}
-
-function awardWinner(winningMove) {
-  Meteor.users.update( { _id: winningMove.uid }, { $inc: { tokens: winningMove.tokens } } );
+/********* Debug *********/
+function log(msg) {
+  console.log("PROTO: " + msg);
 }
